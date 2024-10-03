6d61214f4c9eeb94dba2608988ef5081fa12c188
Polina <pelepolya@yandex.ru>
2024-10-04 00:35:58 +0300

    added list in about

--- lib/src/about.dart
+++ lib/src/about.dart
@@ -1,7 +1,42 @@
 import 'package:flutter/material.dart';
 
-class AboutScreen extends StatelessWidget {
+class AboutScreen extends StatefulWidget {
   const AboutScreen({super.key});
+
+  @override
+  _AboutScreenState createState() => _AboutScreenState();
+}
+
+class _AboutScreenState extends State<AboutScreen> {
+  final List<String> aboutItems = [
+    "Приложение 'Plants' предназначено для любителей растений. Оно помогает расширить знания о ботанике и уходе за растениями.",
+    "С его помощью вы можете находить информацию о ваших любимых растениях и получать советы по их выращиванию.",
+    "Разработчик: Пеленицына П.А.",
+    "Написано на fLuTtEr",
+  ];
+
+  final TextEditingController _textController = TextEditingController();
+
+  void _addNewItem() {
+    setState(() {
+      if (_textController.text.isNotEmpty) {
+        aboutItems.add(_textController.text);
+        _textController.clear();
+      }
+    });
+  }
+
+  void _removeItem(int index) {
+    setState(() {
+      aboutItems.removeAt(index);
+    });
+  }
+
+  @override
+  void dispose() {
+    _textController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -9,33 +44,53 @@ class AboutScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('О приложении'),
       ),
-      body: const Padding(
-        padding: EdgeInsets.all(16.0),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text(
-              'Plants',
-              style: TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
+            Padding(
+              padding: const EdgeInsets.only(bottom: 16.0),
+              child: TextField(
+                controller: _textController,
+                decoration: const InputDecoration(
+                  labelText: 'Добавить новый элемент',
+                  border: OutlineInputBorder(),
+                ),
               ),
             ),
-            SizedBox(height: 10),
-            Text(
-              'Это приложение создано для любителей растений и помогает '
-                  'расширить знания о ботанике и уходе за растениями. '
-                  'Здесь вы можете находить информацию о ваших любимых растениях и '
-                  'получать советы по их уходу.',
-              style: TextStyle(fontSize: 16),
-            ),
-            SizedBox(height: 20),
-            Text(
-              'Версия 1.0.0',
-              style: TextStyle(fontSize: 16, fontStyle: FontStyle.italic),
+            Expanded(
+              child: ListView.separated(
+                itemCount: aboutItems.length,
+                separatorBuilder: (BuildContext context, int index) =>
+                const Divider(
+                  color: Colors.grey,
+                  height: 20,
+                  thickness: 1,
+                ),
+                itemBuilder: (BuildContext context, int index) {
+                  return ListTile(
+                    title: Text(
+                      aboutItems[index],
+                      style: const TextStyle(
+                        fontSize: 16,
+                        color: Colors.black87,
+                      ),
+                    ),
+                    trailing: IconButton(
+                      icon: const Icon(Icons.delete, color: Colors.red),
+                      onPressed: () => _removeItem(index),
+                    ),
+                  );
+                },
+              ),
             ),
           ],
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _addNewItem,
+        backgroundColor: Colors.green,
+        child: const Icon(Icons.add),
       ),
     );
   }
