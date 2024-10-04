d8030e2e61e2991a2e3568857605f9237a247967
Polina <pelepolya@yandex.ru>
2024-10-04 09:33:52 +0300

    fix plantdetails

--- lib/src/plantdetails.dart
+++ lib/src/plantdetails.dart
@@ -2,9 +2,11 @@ import 'package:flutter/material.dart';
 
 class PlantDetailsScreen extends StatefulWidget {
   const PlantDetailsScreen({super.key});
+
   @override
   _PlantDetailsScreenState createState() => _PlantDetailsScreenState();
 }
+
 class _PlantDetailsScreenState extends State<PlantDetailsScreen> {
   List<String> items = [
     "Информация о растении:\nКактусы относятся к семейству суккулентов, способны накапливать воду в стеблях.\n",
@@ -12,11 +14,13 @@ class _PlantDetailsScreenState extends State<PlantDetailsScreen> {
     "Места обитания:\nОбитают в пустынях и полупустынях, предпочитают сухой климат и редкие осадки.\n",
     "Интересные факты:\nНекоторые кактусы могут жить до 200 лет. Цветут кактусы чаще всего ночью.\n",
   ];
+
   void _addNewItem() {
     setState(() {
       items.add("Новая информация о кактусе.\n");
     });
   }
+
   void _removeItem(int index) {
     setState(() {
       if (index >= 0 && index < items.length) {
@@ -24,6 +28,7 @@ class _PlantDetailsScreenState extends State<PlantDetailsScreen> {
       }
     });
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -32,25 +37,31 @@ class _PlantDetailsScreenState extends State<PlantDetailsScreen> {
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: ListView.separated(
-          itemCount: items.length,
-          separatorBuilder: (BuildContext context, int index) => const Divider(
-            color: Colors.grey,
-            height: 20,
-            thickness: 1,
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              for (int i = 0; i < items.length; i++)
+                Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Expanded(
+                          child: Text(
+                            items[i],
+                            style: const TextStyle(fontSize: 16, color: Colors.black87),
+                          ),
+                        ),
+                        IconButton(
+                          icon: const Icon(Icons.delete, color: Colors.red),
+                          onPressed: () => _removeItem(i),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+            ],
           ),
-          itemBuilder: (BuildContext context, int index) {
-            return ListTile(
-              title: Text(
-                items[index],
-                style: const TextStyle(fontSize: 16, color: Colors.black87),
-              ),
-              trailing: IconButton(
-                icon: const Icon(Icons.delete, color: Colors.red),
-                onPressed: () => _removeItem(index),
-              ),
-            );
-          },
         ),
       ),
       floatingActionButton: FloatingActionButton(
